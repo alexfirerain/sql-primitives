c52f452e043356d5d27a28c8874dab5327e19137
alexfirerain <82758912+alexfirerain@users.noreply.github.com>
2022-02-09 14:13:36 +0300

    Three SQL-scripts developed.
    This is very useful for those who want to
     operate on data concerning people with
     name & surname, age, phone and dwelling
     city as well. If you need to get know who
     is from MOSCOW or who is above 27 age,
     you won't seek another solution but this.

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+create table PERSONS (
+    name            varchar(255) not null,
+    surname         varchar(255) not null,
+    age             int check ( age >= 0 ),
+    phone_number    varchar(63) default '-',
+    city_of_living  varchar(255),
+    primary key (name, surname, age)
+);
